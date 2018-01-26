46677eb239a56ec6a65b935fffd8d97460b50908
Quasimodo <quasimodo1660@gmail.com>
2018-01-26 10:32:08 -0800

    Finished Add event and display table

---
+++ Blet Exam/AddItemViewController.swift
@@ -0,0 +1,51 @@
+//
+//  AddItemViewController.swift
+//  Blet Exam
+//
+//  Created by Peisure on 1/26/18.
+//  Copyright © 2018 Ben. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UITextField!
+    @IBOutlet weak var contentLabel: UITextView!
+    @IBOutlet weak var timePick: UIDatePicker!
+    
+    weak var delegate:AddViewDelegate?
+    var indexPath:NSIndexPath?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        delegate?.addItem(self, with: titleLabel.text!, contentLabel.text, and: timePick.date, by: sender, at: indexPath)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+protocol AddViewDelegate:class {
+    func addItem(_ controller:AddItemViewController, with toDoTitle:String,_ content:String,and beginTime:Date, by sender: UIButton, at indexPath:NSIndexPath?)
+}
---
+++ Blet Exam/ShowTableViewController.swift
@@ -0,0 +1,180 @@
+//
+//  ViewController.swift
+//  Blet Exam
+//
+//  Created by Peisure on 1/25/18.
+//  Copyright © 2018 Ben. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ShowTableViewController: UITableViewController, AddViewDelegate, cellDelegate{
+    
+    
+    
+    
+    func editRow(_ sender: ICell) {
+        print("here")
+    }
+    
+    
+    let manageDatabase = (UIApplication.shared.delegate as!
+        AppDelegate).persistentContainer.viewContext
+    
+    
+    var com = [Todolist]()
+    var incom = [Todolist]()
+    
+    //****************  Display table **********************************
+    var sectionTitle = ["Incomplete","Complete"]
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionTitle.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitle[section]
+    }
+
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0{
+            return incom.count
+        }
+        else{
+            return com.count
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ICell") as! ICell
+            cell.displayTitle.text = incom[indexPath.row].title
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            let myTime = incom[indexPath.row].beginDate
+            cell.displayTime.text = formatter.string(from: myTime!)
+            cell.delegate = self
+            return cell
+        }
+        else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CCell") as! CCell
+            cell.displayTitle.text = com[indexPath.row].title
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            let myTime = com[indexPath.row].beginDate
+            cell.displayTime.text = formatter.string(from: myTime!)
+            return cell
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    //***************** add, edit data  *********************************
+    
+    func addItem(_ controller: AddItemViewController, with toDoTitle: String, _ content: String, and beginTime: Date, by sender: UIButton, at indexPath: NSIndexPath?) {
+        if sender.tag == 0 {
+            self.navigationController?.popViewController(animated: true)
+            dismiss(animated: true, completion: nil)
+        }
+        else{
+            let item = NSEntityDescription.insertNewObject(forEntityName: "Todolist", into: manageDatabase) as! Todolist
+            item.title = toDoTitle
+            item.content = content
+            item.beginDate = beginTime
+            item.finish = false
+            incom.append(item)
+            do{
+                try manageDatabase.save()
+            }
+            catch{
+                print("\(error)")
+            }
+            self.navigationController?.popViewController(animated: true)
+            dismiss(animated: true, completion: nil)
+            fetchAllItems()
+            tableView.reloadData()
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "addItem"{
+            let target = segue.destination as! AddItemViewController
+            target.delegate = self
+        }
+       
+    }
+    
+    //*********************************************************************
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchAllItems()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func fetchAllItems(){
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Todolist")
+        request.predicate = NSPredicate(format: "finish == %@", NSNumber(value: true))
+        do{
+            let result = try manageDatabase.fetch(request)
+            com = result as! [Todolist]
+        }
+        catch{
+            print("\(error)")
+        }
+        request.predicate = NSPredicate(format: "finish == %@", NSNumber(value: false))
+        do{
+            let result = try manageDatabase.fetch(request)
+            incom = result as! [Todolist]
+        }
+        catch{
+            print("\(error)")
+        }
+    }
+    
+    
+    
+}
+
+class CCell:UITableViewCell{
+    @IBOutlet weak var displayTitle: UILabel!
+    @IBOutlet weak var displayTime: UILabel!
+}
+
+
+class ICell:UITableViewCell{
+    @IBOutlet weak var displayTime: UILabel!
+    @IBOutlet weak var displayTitle: UILabel!
+    weak var delegate:cellDelegate?
+    
+    @IBAction func editButtonPressed(_ sender: UIButton) {
+        delegate?.editRow(self)
+    }
+    
+}
+
+protocol cellDelegate:class {
+   func editRow(_ sender:ICell)
+}
+
+
+
+
+
+
+
+
